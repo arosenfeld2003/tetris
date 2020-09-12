bd8f9d53b61c3bdb7b91045b6ae987c1e01512e7
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-09-11 23:33:15 -0700

    working tetris game

--- playfield.js
+++ playfield.js
@@ -2,19 +2,93 @@
 // We are 'hiding' the top 20 rows, so the board will apear 10 x 20
 
 class Board {
-  constructor(ctx) {
+  constructor(ctx, ctxNext) {
     this.ctx = ctx;
-    this.grid = this.getEmptyBoard();
+    this.ctxNext = ctxNext;
+    this.init();
   }
-  
-  // Get matrix filled with zeros.
-  getEmptyBoard() {
-    return Array.from(
-      // Create a 2D array (ROWS x COLS)
-      // {length: ROWS} is an iteratble array-like object
-      // mapFn creates the new array for each column
-      {length: ROWS}, () => Array(COLS).fill(0)
-    );
+
+  init() {
+    // Calculate size of canvas from constants.
+    this.ctx.canvas.width = COLS * BLOCK_SIZE;
+    this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+    // Scale so we don't need to give size on every draw.
+    this.ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+  }
+
+  reset() {
+    this.grid = this.getEmptyGrid();
+    this.piece = new Piece(this.ctx);
+    this.piece.setStartingPosition();
+    this.getNewPiece();
+  }
+
+  getNewPiece() {
+    const { width, height } = this.ctxNext.canvas;
+    this.next = new Piece(this.ctxNext);
+    this.ctxNext.clearRect(0, 0, width, height);
+    this.next.draw();
+  }
+
+  draw() {
+    this.piece.draw();
+    this.drawBoard();
+  }
+
+  drop() {
+    let p = moves[KEY.DOWN](this.piece);
+    if (this.valid(p)) {
+      this.piece.move(p);
+    } else {
+      this.freeze();
+      this.clearLines();
+      if (this.piece.y === 0) {
+        // Game over
+        return false;
+      }
+      this.piece = this.next;
+      this.piece.ctx = this.ctx;
+      this.piece.setStartingPosition();
+      this.getNewPiece();
+    }
+    return true;
+  }
+
+  clearLines() {
+    let lines = 0;
+
+    this.grid.forEach((row, y) => {
+      // If every value is greater than zero then we have a full row.
+      if (row.every((value) => value > 0)) {
+        lines++;
+
+        // Remove the row.
+        this.grid.splice(y, 1);
+
+        // Add zero filled row at the top.
+        this.grid.unshift(Array(COLS).fill(0));
+      }
+    });
+
+    if (lines > 0) {
+      // Calculate points from cleared lines and level.
+
+      account.score += this.getLinesClearedPoints(lines);
+      account.lines += lines;
+
+      // If we have reached the lines for next level
+      if (account.lines >= LINES_PER_LEVEL) {
+        // Goto next level
+        account.level++;
+
+        // Remove lines so we start working for the next level
+        account.lines -= LINES_PER_LEVEL;
+
+        // Increase speed of game
+        time.level = LEVEL[account.level];
+      }
+    }
   }
 
   valid(p) {
@@ -25,6 +99,31 @@ class Board {
         return value === 0 || (this.isInsideWalls(x, y) && this.notOccupied(x, y));
       });
     });
+  }
+
+  freeze() {
+    this.piece.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.grid[y + this.piece.y][x + this.piece.x] = value;
+        }
+      });
+    });
+  }
+
+  drawBoard() {
+    this.grid.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.ctx.fillStyle = COLORS[value];
+          this.ctx.fillRect(x, y, 1, 1);
+        }
+      });
+    });
+  }
+
+  getEmptyGrid() {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
   }
 
   isInsideWalls(x, y) {
@@ -55,5 +154,19 @@ class Board {
 
     return p;
   }
-}
 
+  getLinesClearedPoints(lines, level) {
+    const lineClearPoints =
+      lines === 1
+        ? POINTS.SINGLE
+        : lines === 2
+        ? POINTS.DOUBLE
+        : lines === 3
+        ? POINTS.TRIPLE
+        : lines === 4
+        ? POINTS.TETRIS
+        : 0;
+
+    return (account.level + 1) * lineClearPoints;
+  }
+}
--- tetrominos.js
+++ tetrominos.js
@@ -17,9 +17,6 @@ class Piece {
     this.ctx.fillStyle = this.color;
     this.shape.forEach((row, y) => {
       row.forEach((value, x) => {
-        // this.x, this.y gives the left upper position of the shape
-        // x, y gives the position of the block in the shape
-        // this.x + x is then the position of the block on the board
         if (value > 0) {
           this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
         }
@@ -32,6 +29,7 @@ class Piece {
       this.x = p.x;
       this.y = p.y;
     }
+    this.shape = p.shape;
   }
 
   hardDrop() {
@@ -46,4 +44,3 @@ class Piece {
     return Math.floor(Math.random() * noOfTypes + 1);
   }
 }
-
